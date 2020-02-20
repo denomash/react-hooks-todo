0245c260728682beb97b5657f553f150d359cbb8
Dennis Macharia <dennismacharia4747@gmail.com>
2020-02-20 23:50:22 +0300

    add remove todo functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const Todo = ({ todo, index }) => {
-  return <div className="todo">{todo.text}</div>;
+const Todo = ({ todo, index, completeTodo, removeTodo }) => {
+  return (
+    <div
+      style={{ textDecoration: todo.isComplete ? 'line-through' : '' }}
+      className="todo"
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>X</button>
+      </div>
+    </div>
+  );
 };
 
 const TodoForm = ({ addTodo }) => {
@@ -48,11 +59,30 @@ const App = () => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
   };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isComplete = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
